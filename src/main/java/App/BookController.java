080d6f64483250d28f72df25cd16caf7e0f64dd9
ksoethwel9220 <auroralunaria20@gmail.com>
2026-08-10 14:44:26 +0630

    built update and delete methods.

--- src/main/java/App/BookController.java
+++ src/main/java/App/BookController.java
@@ -3,10 +3,14 @@ package App;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,6 +32,16 @@ public class BookController {
 	@PostMapping
 	public void addNewBook(@RequestBody Books books){
 		bookService.addBook(books);
+	}
+	
+	@PutMapping(path = "{id}")
+	public void updateBook(@PathVariable("id") Long id,@RequestParam String name,@RequestParam Double price,@RequestParam Long quantity) {
+		bookService.updateBook(id, name, price, quantity);
+	}
+	
+	@DeleteMapping(path = "{id}")
+	public void deleteBook(@PathVariable("id") Long id) {
+		bookService.deleteBook(id);
 	}
 
 }
--- src/main/java/App/BookService.java
+++ src/main/java/App/BookService.java
@@ -22,4 +22,26 @@ public class BookService{
 		bookRepository.save(books);
 	}
 
+	public void deleteBook(Long id) {
+		boolean exit = bookRepository.existsById(id);
+		if(!exit) {
+			throw new IllegalStateException("This id " + id + " is not exited.");
+		}
+		bookRepository.deleteById(id);
+	}
+
+	public void updateBook(Long id, String name, Double price, Long quantity) {
+		Books books = bookRepository.findById(id).orElseThrow (()-> new IllegalStateException("This id ;" + id + " is not exited."));
+		if(name != null && name.length() > 0) {
+			books.setName(name);
+		}
+		if(price != null && (price !=0 || price > 0)){
+			books.setPrice(price);
+		}
+		if(quantity > 0) {
+			books.setQuantity(quantity);
+		}
+		bookRepository.save(books);
+	}
+
 }
